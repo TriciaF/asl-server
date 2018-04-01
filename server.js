dd1cbdf6267664e73500273b9e6459e1034d5372
TriciaF <TriciaF@thinkful.com>
2018-04-01 17:01:48 -0400

    turned off the CORS in the server"

--- server.js
+++ server.js
@@ -22,22 +22,24 @@ app.use(
 		skip: (req, res) => process.env.NODE_ENV === 'test'
 	})
 );
-app.use(
-	cors({
-		origin: CLIENT_ORIGIN
-	})
-);
-// ifor CORS
-app.use(function(req, res, next) 
-{
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-	if (req.method === 'OPTIONS') {
-		return res.send(204);
-	}
-	next();
-});
+
+//Turn this section 'on' if doing local development
+// app.use(
+// 	cors({
+// 		origin: CLIENT_ORIGIN
+// 	})
+// );
+// // ifor CORS
+// app.use(function(req, res, next) 
+// {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+// 	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+// 	if (req.method === 'OPTIONS') {
+// 		return res.send(204);
+// 	}
+// 	next();
+// });
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
