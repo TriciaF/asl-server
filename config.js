9b28882a8fc901c93380a7f77c429aad0db4e953
TriciaF <TriciaF@thinkful.com>
2018-04-02 09:25:07 -0400

    added .travis.yml file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	PORT: process.env.PORT || 8080,
-	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://asl-learning.netlify.com',
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://learning-asl-alphabet.netlify.com',
 	DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://TriciaF:tjandsam01@ds229549.mlab.com:29549/asl_questions',
 	TEST_DATABASE_URL:
