4d9a2a8eeac06d36a7a81016cbef0b0fe289b9f7
TriciaF <TriciaF@thinkful.com>
2020-12-19 19:56:55 -0500

    updated username for database access

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 	PORT: process.env.PORT || 8080,
 	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://learning-asl-alphabet.netlify.com',
 	DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb+srv://palforrester@gmail.com:SandTrap64*@asl-questions.miz7i.mongodb.net/asl_questions?retryWrites=true&w=majority',
+        process.env.DATABASE_URL || 'mongodb+srv://TriciaF:SandTrap64*@asl-questions.miz7i.mongodb.net/asl_questions?retryWrites=true&w=majority',
 	TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'http://localhost/asl-question-db-test',
